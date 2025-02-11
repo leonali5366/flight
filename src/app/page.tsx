64f0ea17cb9d7b408b5da0ffddd6bfd2ec087dee
LeonAli5366 <leonali274784@gmail.com>
2025-02-11 13:45:38 +0600

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
         </h1>
         <p className="mb-8 text-sm tracking-wider opacity-60">
           {lang === "french"
-            ? "Saisissez les détails de votre vol ci-dessous, et notre équipe vous contactera dans les plus brefs délais. Les tarifs des vols charters en jet privé varient selon le marché et commencent à partir de 11 000 $."
+            ? "Saisissez les détails de votre vol ci-dessous, et notre équipe vous contactera dans les plus brefs délais. Les tarifs des vols charters en jet privé varient selon le marché et commencent à partir de €5 000 euros."
             : "Enter your flight details below, and our team will get in touch with you as soon as possible. Private jet charter prices are subject to market rates and start from €5,000 euros."}
         </p>
         <Form />
